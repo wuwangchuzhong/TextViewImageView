9fb80d4f308772aa2d8bc7a9bffc4d583b95e8c2
miaoweiwei <15234276528@163.com>
2018-01-26 11:31:47 +0800

    first_VerticlaImageSpanextends

---
+++ VerticalImageSpanextends.java
@@ -0,0 +1,58 @@
+package com.yidian.buyer.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.text.style.ImageSpan;
+
+/**
+ * Created by miao on 2017/10/30.
+ * 自定义ImageSpan图片居中
+ */
+
+public class VerticalImageSpanextends extends ImageSpan {
+
+    public VerticalImageSpanextends(Drawable drawable) {
+        super(drawable);
+    }
+
+    public VerticalImageSpanextends(Context context, @DrawableRes int resourceId) {
+        super(context, resourceId, ALIGN_BOTTOM);
+    }
+
+
+    public int getSize(Paint paint, CharSequence text, int start, int end,
+                       Paint.FontMetricsInt fontMetricsInt) {
+        Drawable drawable = getDrawable();
+        Rect rect = drawable.getBounds();
+        if (fontMetricsInt != null) {
+            Paint.FontMetricsInt fmPaint = paint.getFontMetricsInt();
+            int fontHeight = fmPaint.bottom - fmPaint.top;
+            int drHeight = rect.bottom - rect.top;
+
+            int top = drHeight / 2 - fontHeight / 4;
+            int bottom = drHeight / 2 + fontHeight / 4;
+
+            fontMetricsInt.ascent = -bottom;
+            fontMetricsInt.top = -bottom;
+            fontMetricsInt.bottom = top;
+            fontMetricsInt.descent = top;
+        }
+        return rect.right;
+    }
+
+    @Override
+    public void draw(Canvas canvas, CharSequence text, int start, int end,
+                     float x, int top, int y, int bottom, Paint paint) {
+        Drawable drawable = getDrawable();
+        canvas.save();
+        int transY = 0;
+        transY = ((bottom - top) - drawable.getBounds().bottom) / 2 + top;
+        canvas.translate(x, transY);
+        drawable.draw(canvas);
+        canvas.restore();
+    }
+}
